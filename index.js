6e1647ae7c26028aecabf73c0a802ffde5181513
crowwan <crowwan@naver.com>
2022-10-12 19:07:21 +0900

    mongoURI 분리

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const { User } = require('./models/User');
 
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://crowwan:crowlast1215@boilerplate.lugyz4e.mongodb.net/?retryWrites=true&w=majority',{}).then(()=>console.log('mongodb connected'))
+const config = require('./config/key');
+mongoose.connect(config.mongoURI,{}).then(()=>console.log('mongodb connected'))
 .catch((err)=>console.log(err));
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get('/',(req,res)=>res.send('hello world!'));
+app.post('/register',(req,res)=>{
+  const user = new User(req.body);
+
+  user.save((err, userInfo)=>{
+    if(err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success:true
+    })
+  });
+});
 
 app.listen(port,()=>console.log(`example app listening on ${port}`));
