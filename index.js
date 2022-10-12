1273d42aba9e0032aa08ca3dff041eb26d716fb2
crowwan <crowwan@naver.com>
2022-10-12 19:39:06 +0900

    로그인 구현 1

--- index.js
+++ index.js
@@ -23,4 +23,28 @@ app.post('/register',(req,res)=>{
   });
 });
 
+app.post('/login',(req,res)=>{
+  //요청된 이메일을 데이터베이스에 있는지 찾음
+  User.findOne({ email: req.body.email },(err,user)=>{
+    if(!user){
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      });
+    }
+    // 이메일에 대한 비밀번호가 같은지 확인한다.
+    
+    user.comparePassword(req.body.password, (err, isMatch)=>{
+      if(!isMatch)
+        return res.json({ loginSuccess: false, message: '비밀번호가 틀렸습니다.'})
+      
+      //비밀번호가 맞으면 토근 생성
+      user.generateToken((err,user)=>{
+        
+      });
+    });
+  });
+  
+});
+
 app.listen(port,()=>console.log(`example app listening on ${port}`));
